eb296fd8e10ba3c41db86674a379ce468b1458d1
Ayman <87582743+mastergive514@users.noreply.github.com>
2024-01-06 21:28:42 +0100

    Update CmdMain.cs

--- MCGalaxy/Commands/World/CmdMain.cs
+++ MCGalaxy/Commands/World/CmdMain.cs
@@ -15,6 +15,8 @@
     or implied. See the Licenses for the specific language governing
     permissions and limitations under the Licenses.
  */
+using System;
+
 using MCGalaxy.Games;
 
 namespace MCGalaxy.Commands.World {
